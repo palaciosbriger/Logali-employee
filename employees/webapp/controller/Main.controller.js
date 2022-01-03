fd103581649d39670b7281111ea05dc7468d6f3f
palaciosbriger <palaciosbriger@gmail.com>
2022-01-03 17:13:59 +0000

    First commint 04

--- employees/webapp/controller/Main.controller.js
+++ employees/webapp/controller/Main.controller.js
@@ -10,6 +10,20 @@ sap.ui.define([
         return Controller.extend("logaligroup.employees.controller.Main", {
             onInit: function () {
 
+            },
+            onValidate: function(){
+                var inputEmployee = this.byId("inputEmployee");
+                var valueEmployee = inputEmployee.getValue();
+
+                if(valueEmployee.length===6){
+                    //inputEmployee.setDescription("OK");
+                    this.getView().byId("labelCountry").setVisible(true);
+                    this.getView().byId("slCountry").setVisible(true);
+                }else{
+                    //inputEmployee.setDescription("Not OK");
+                    this.getView().byId("labelCountry").setVisible(false);
+                    this.getView().byId("slCountry").setVisible(false);
+                }
             }
         });
     });
