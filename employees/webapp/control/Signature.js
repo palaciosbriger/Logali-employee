0f0bc408c3ddea40d96f1f9d686914462d59cd39
palaciosbriger <palaciosbriger@gmail.com>
2022-02-03 13:36:08 +0000

    Odata y Objetos Media

--- employees/webapp/control/Signature.js
+++ employees/webapp/control/Signature.js
@@ -5,28 +5,28 @@ sap.ui.define([
     function (Control) {
         return Control.extend("logaligroup.employees.controller.Signature", {
 
-        metadata: {
-            properties:{
-                "width" :{
-                    type: "sap.ui.core.CSSSize",
-                    defaultValue: "400px"
-                },
-                "height" :{
-                    type: "sap.ui.core.CSSSize",
-                    defaultValue: "100px"
-                },
-                "bgcolor" :{
-                    type: "sap.ui.core.CSSColor",
-                    defaultValue: "white"
-                }                               
-            }
-        },
+            metadata: {
+                properties: {
+                    "width": {
+                        type: "sap.ui.core.CSSSize",
+                        defaultValue: "400px"
+                    },
+                    "height": {
+                        type: "sap.ui.core.CSSSize",
+                        defaultValue: "100px"
+                    },
+                    "bgcolor": {
+                        type: "sap.ui.core.CSSColor",
+                        defaultValue: "white"
+                    }
+                }
+            },
 
             onInit: function () {
 
             },
 
-            renderer: function(oRM, oControl) {
+            renderer: function (oRM, oControl) {
                 oRM.write("<div");
                 oRM.addStyle("width", oControl.getProperty("width"));
                 oRM.addStyle("height", oControl.getProperty("height"));
@@ -34,24 +34,41 @@ sap.ui.define([
                 oRM.addStyle("border", "1px solid black");
                 oRM.writeStyles();
                 oRM.write(">");
-                oRM.write("<canvas width='" + oControl.getProperty("width") + "' " + "height= '" 
-                          + oControl.getProperty("height") + "'");
+                oRM.write("<canvas width='" + oControl.getProperty("width") + "' " + "height= '"
+                    + oControl.getProperty("height") + "'");
 
-                 oRM.write("></cavas>");
-                 oRM.write("</div>");          
+                oRM.write("></cavas>");
+                oRM.write("</div>");
             },
 
             onAfterRendering: function () {
                 var canvas = document.querySelector("canvas");
                 try {
-                    this.signaturePad = new SignaturePad(canvas);    
-                } catch(e){
+                    this.signaturePad = new SignaturePad(canvas);
+                    this.signaturePad.fill = false;
+                    canvas.addEventListener("mousedown", function () {
+                        this.signaturePad.fill = true;
+                    }.bind(this));
+                } catch (e) {
                     console.error(e);
                 }
             },
 
             clear: function () {
                 this.signaturePad.clear();
+                this.signaturePad.fill = false;
+            },
+
+            isFill: function () {
+                return this.signaturePad.fill;
+            },
+
+            getSignature: function () {
+                return this.signaturePad.toDataURL();
+            },
+
+            setSignature: function (signature) {
+                this.signaturePad.fromDataURL(signature);
             }
         });
     }); 
